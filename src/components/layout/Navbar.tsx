679f1e052c8601041d6d10748d0e5cbfb7481d5f
Amir <amir.kk1999@gmail.com>
2026-01-01 19:22:08 +0330

    ➕ feat(ui): Add shadcn Button component to project

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
     return (
         <nav className="h-12">
-            <section className="max-w-[1240px] mx-auto h-full">
+            <section className="max-w-[1240px] mx-auto h-full p-2">
 
             </section>
         </nav>
