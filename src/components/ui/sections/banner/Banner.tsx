87f6bc6fa53b50f5851641c2aa40220531fee92d
Amir <amir.kk1999@gmail.com>
2026-01-02 23:44:50 +0330

    🛠 chore(assets): Clean public directory and add personal images

--- src/components/ui/sections/banner/Banner.tsx
+++ src/components/ui/sections/banner/Banner.tsx
@@ -1,7 +1,11 @@
 export const Banner = () => {
     return (
-        <section className="max-w-3xl mx-auto h-20 py-2 px-5">
+        <section className="max-w-3xl py-2 px-5 mx-auto flex flex-col">
+            <div className="">
 
+            </div>
+            <div className="">
+            </div>
         </section>
     );
 };
