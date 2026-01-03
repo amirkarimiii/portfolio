e438c134a5d56edbd94c0f7a9905bb94865f45b1
Amir <amir.kk1999@gmail.com>
2026-01-03 21:08:08 +0330

    ➕ feat(ui): Add tab to InfoSection.tsx

--- src/components/ui/sections/information/infoSection.tsx
+++ src/components/ui/sections/information/infoSection.tsx
@@ -1,10 +1,26 @@
 import {Separator} from "@/components/ui/shadcn/separator";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/shadcn/tabs";
 
 export function InfoSection() {
     return (
         <section className="max-w-4xl mx-auto">
-            <div className="py-2 px-5 flex flex-col gap-10 lg:flex-row-reverse mb-10 lg:mt-20">
+            <div className="py-2 px-5 mb-10">
+                <Tabs defaultValue="focus">
+                    <TabsList className="">
+                        <TabsTrigger value="focus">🧠 Technical Focus</TabsTrigger>
+                        <TabsTrigger value="language">🗣 Language</TabsTrigger>
+                        <TabsTrigger value="education">🎓 Educational Background</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="focus">
 
+                    </TabsContent>
+                    <TabsContent value="language">
+
+                    </TabsContent>
+                    <TabsContent value="education">
+
+                    </TabsContent>
+                </Tabs>
             </div>
             <Separator/>
         </section>
