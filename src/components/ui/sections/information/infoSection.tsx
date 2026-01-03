b1f7a8f75848193ede75835888f982ec573abe2e
Amir <amir.kk1999@gmail.com>
2026-01-03 23:25:49 +0330

    ➕ feat(ui): Complete content for Language tab

--- src/components/ui/sections/information/infoSection.tsx
+++ src/components/ui/sections/information/infoSection.tsx
@@ -1,5 +1,8 @@
 import {Separator} from "@/components/ui/shadcn/separator";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/shadcn/tabs";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/shadcn/alert";
+import {BadgeCheck} from "lucide-react";
+import Image from "next/image";
 
 export function InfoSection() {
     return (
@@ -35,7 +38,54 @@ export function InfoSection() {
                         </ul>
                     </TabsContent>
                     <TabsContent value="language">
-
+                        <h2 className="font-bold text-xl mt-3 lg:mt-5 lg:text-3xl">🗣 Language</h2>
+                        <p className="mt-10">
+                            <span className="font-bold text-base lg:text-xl">I&apos;m a persian</span><br/>
+                            <span className="text-sm lg:text-base">and my native language is persian.
+                                <br/><br/>and also I&apos;m skilled in</span>
+                        </p>
+                        <div className="flex flex-col gap-3 mt-3 md:flex-row">
+                            <Alert variant="default">
+                                <AlertTitle className="text-base lg:text-xl">English</AlertTitle>
+                                <AlertDescription className="lg:text-base">
+                                    <ul className="list-disc ml-5 mt-5">
+                                        <li className="mt-1">
+                                            <strong>Reading:</strong> Comfortable reading and understanding English documentation and technical texts with ease.
+                                        </li>
+                                        <li className="mt-1">
+                                            <strong>Listening:</strong> Strong listening skills; 95% of the YouTube content I consume is in English.
+                                        </li>
+                                        <li className="mt-1">
+                                            <strong>Speaking:</strong> Able to hold conversations in English, though I feel slightly nervous due to limited recent practice with speaking partners.
+                                        </li>
+                                        <li className="mt-1">
+                                            <strong>Writing:</strong> Solid writing skills at a similar level to my speaking ability, with room for rapid improvement through practice.
+                                        </li>
+                                    </ul>
+                                </AlertDescription>
+                            </Alert>
+                            <Alert variant="default">
+                                <AlertTitle className="text-base lg:text-xl">Français</AlertTitle>
+                                <AlertDescription className="lg:text-base">
+                                    Globalement, je suis débutant en français, mais je suis capable de communiquer efficacement. Mes compétences en compréhension écrite (reading), expression écrite (writing), expression orale (speaking) et compréhension orale (listening) progressent de manière parallèle et équilibrée.
+                                    <div className="flex gap-2">
+                                        <div className="relative mt-5 rounded-lg overflow-hidden flex-1 h-30 max-w-30">
+                                            <Image src="/duolingo.png"
+                                                   alt=""
+                                                   fill
+                                            />
+                                        </div>
+                                        <div className="flex-1">
+                                            <div className="flex flex-1 py-2 gap-3">
+                                                <h3 className="font-bold w-22 lg:w-28 my-auto">Score actuel sur Duolingo</h3>
+                                                <p className="font-bold text-4xl">93</p>
+                                            </div>
+                                            <p className="text-xs"><strong>À noter :</strong> j’apprends le français à partir de l’anglais, car Duolingo ne prend pas encore en charge le persan.</p>
+                                        </div>
+                                    </div>
+                                </AlertDescription>
+                            </Alert>
+                        </div>
                     </TabsContent>
                     <TabsContent value="education">
 
