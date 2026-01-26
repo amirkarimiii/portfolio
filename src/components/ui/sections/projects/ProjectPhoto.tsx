580c46320bddede7cc82a1f54e5a0feb74bdccc5
Amir <amir.kk1999@gmail.com>
2026-01-26 20:23:41 +0330

    ➕ feat(projects): add 'tap to see {theme} mode' badge below project images

--- src/components/ui/sections/projects/ProjectPhoto.tsx
+++ src/components/ui/sections/projects/ProjectPhoto.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 
 import {Card} from "@/components/ui/shadcn/card";
 import {useState} from "react";
+import {Badge} from "@/components/ui/shadcn/badge";
 
 function ProjectPhoto() {
 
     const [theme, setTheme] = useState("light");
 
     return (
-            <Card className="p-5" onClick={(e) =>
-                theme === "light" ? setTheme("dark") : setTheme("light")
+        <Card className="p-5" onClick={(e) =>
+            theme === "light" ? setTheme("dark") : setTheme("light")
         }>
             <div className="relative w-full aspect-[1.62] mx-auto select-none">
                 <Image
@@ -19,6 +20,7 @@ function ProjectPhoto() {
                     fill
                 />
             </div>
+            <Badge className="block mx-auto mt-5">{`tap to see ${theme === "light" ? "dark" : "light"} mode 👆🏻`}</Badge>
         </Card>
     );
 }
