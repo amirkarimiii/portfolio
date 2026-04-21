c85793477b2899e75fc35ec0eedb3bc423f34b5b
Amir <amir.kk1999@gmail.com>
2026-04-21 20:15:14 +0330

    🐞 fix(ProjectPhoto): fix hydration error

--- src/components/ui/sections/projects/ProjectPhoto.tsx
+++ src/components/ui/sections/projects/ProjectPhoto.tsx
@@ -2,46 +2,56 @@
 import Image from "next/image";
 
 import {Card} from "@/components/ui/shadcn/card";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Badge} from "@/components/ui/shadcn/badge";
 import {useTheme} from "next-themes";
 import {Skeleton} from "@/components/ui/shadcn/skeleton";
 
+
 function ProjectPhoto() {
 
     const {resolvedTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
     const [theme, setTheme] = useState(resolvedTheme);
+
+
+    useEffect(() => {
+        setMounted(true);
+        if (resolvedTheme) {
+            setTheme(resolvedTheme as "dark" | "light");
+        }
+    }, [resolvedTheme]);
+
+
+    if (!mounted) return (
+        <Card className="p-5">
+            <Skeleton className="w-full aspect-[1.62]"/>
+            <Badge className="block mx-auto mt-5">{`tap to see dark/light mode 👆🏻`}</Badge>
+        </Card>
+    );
 
     return (
         <Card className="p-5" onClick={(e) =>
             theme === "light" ? setTheme("dark") : setTheme("light")
         }>
-            {
-                resolvedTheme ?
-                    <>
-                        <div
-                            className={`relative w-full aspect-[1.62] mx-auto select-none cursor-pointer ${theme === "light" ? "hidden" : ""}`}>
-                            <Image
-                                src={`/projects/cryptology/dark.png`}
-                                alt={`cryptology screenshots dark`}
-                                fill
-                                preload
-                            />
-                        </div>
-                        <div
-                            className={`relative w-full aspect-[1.62] mx-auto select-none cursor-pointer ${theme === "dark" ? "hidden" : ""}`}>
-                            <Image
-                                src={`/projects/cryptology/light.png`}
-                                alt={`cryptology screenshots light`}
-                                fill
-                                preload
-                            />
-                        </div>
-                    </> :
-                    <>
-                        <Skeleton className="w-full"></Skeleton>
-                    </>
-            }
+            <div
+                className={`relative w-full aspect-[1.62] mx-auto select-none cursor-pointer ${theme === "light" ? "hidden" : ""}`}>
+                <Image
+                    src={`/projects/cryptology/dark.png`}
+                    alt={`cryptology screenshots dark`}
+                    fill
+                    preload
+                />
+            </div>
+            <div
+                className={`relative w-full aspect-[1.62] mx-auto select-none cursor-pointer ${theme === "dark" ? "hidden" : ""}`}>
+                <Image
+                    src={`/projects/cryptology/light.png`}
+                    alt={`cryptology screenshots light`}
+                    fill
+                    preload
+                />
+            </div>
 
             <Badge className="block mx-auto mt-5">{`tap to see ${theme === "light" ? "dark" : "light"} mode 👆🏻`}</Badge>
         </Card>
