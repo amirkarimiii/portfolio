b3f29e7ad07cec2342c3859f06ea4a5374d2b5bc
Amir <amir.kk1999@gmail.com>
2026-04-21 18:31:31 +0330

    🐞 fix(ProjectPhoto): fix preloading project images

--- src/components/ui/sections/projects/ProjectPhoto.tsx
+++ src/components/ui/sections/projects/ProjectPhoto.tsx
@@ -13,11 +13,20 @@ function ProjectPhoto() {
         <Card className="p-5" onClick={(e) =>
             theme === "light" ? setTheme("dark") : setTheme("light")
         }>
-            <div className="relative w-full aspect-[1.62] mx-auto select-none cursor-pointer">
+            <div className={`relative w-full aspect-[1.62] mx-auto select-none cursor-pointer ${theme === "light" ? "hidden" : ""}`}>
                 <Image
-                    src={`/projects/cryptology/${theme}.png`}
-                    alt={`cryptology screenshots ${theme}`}
+                    src={`/projects/cryptology/dark.png`}
+                    alt={`cryptology screenshots dark`}
                     fill
+                    preload
+                />
+            </div>
+            <div className={`relative w-full aspect-[1.62] mx-auto select-none cursor-pointer ${theme === "dark" ? "hidden" : ""}`}>
+                <Image
+                    src={`/projects/cryptology/light.png`}
+                    alt={`cryptology screenshots light`}
+                    fill
+                    preload
                 />
             </div>
             <Badge className="block mx-auto mt-5">{`tap to see ${theme === "light" ? "dark" : "light"} mode 👆🏻`}</Badge>
