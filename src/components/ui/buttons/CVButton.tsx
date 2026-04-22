940934888ac520abdad058428a02f141b46e4253
Amir <amir.kk1999@gmail.com>
2026-04-22 18:48:12 +0330

    ➕ feat(CVButton): made get cv button functional

--- src/components/ui/buttons/CVButton.tsx
+++ src/components/ui/buttons/CVButton.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import {Button} from "@/components/ui/shadcn/button";
+import {ExternalLink} from "lucide-react";
+import Link from "next/link";
 
 export function CvButton() {
     return (
@@ -8,7 +10,13 @@ export function CvButton() {
             variant="outline"
             className="w-25 h-max rounded-md p-1 cursor-pointer"
         >
-            Get My CV
+            <Link
+                href="https://drive.google.com/file/d/1yN2KR-Lc0RoDthUUwt_tiHn-6a7A_pHA/view?usp=sharing"
+                target="_blank"
+                className="flex"
+            >
+                Get My CV
+            </Link>
         </Button>
     );
 }
