65a623c3c9a4901191bac7c2d3fb8aff721ccd49
Amir <amir.kk1999@gmail.com>
2026-04-22 18:06:51 +0330

    ➕ feat(CVButton): add CVButton to navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,12 @@
 import {ThemeButton} from "@/components/ui/buttons/ThemeButton";
+import {CvButton} from "@/components/ui/buttons/CVButton";
 
 export const Navbar = () => {
     return (
         <nav className="h-12">
-            <section className="max-w-4xl mx-auto h-full py-2 px-5">
+            <section className="max-w-4xl mx-auto h-full py-2 px-5 flex justify-between">
                 <ThemeButton/>
+                <CvButton/>
             </section>
         </nav>
     );
---
+++ src/components/ui/buttons/CVButton.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {Button} from "@/components/ui/shadcn/button";
+
+export function CvButton() {
+    return (
+        <Button
+            variant="outline"
+            className="w-25 h-max rounded-md p-1 cursor-pointer"
+        >
+            Get My CV
+        </Button>
+    );
+}
+
