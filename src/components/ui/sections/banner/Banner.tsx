2d83902b7f870db93afd6a11524e77325a7be8d3
Amir <amir.kk1999@gmail.com>
2026-04-22 17:22:49 +0330

    🔄 refactor(Banner): Better layout of banner buttons

--- src/components/ui/sections/banner/Banner.tsx
+++ src/components/ui/sections/banner/Banner.tsx
@@ -1,5 +1,5 @@
 import {MyImage} from "@/components/ui/sections/banner/MyImage";
-import {BadgeCheck} from "lucide-react";
+import {BadgeCheck, ExternalLink} from "lucide-react";
 import {Alert, AlertDescription, AlertTitle} from "@/components/ui/shadcn/alert";
 import {Separator} from "@/components/ui/shadcn/separator";
 import {Button} from "@/components/ui/shadcn/button";
@@ -10,7 +10,7 @@ export const Banner = () => {
         <section className="max-w-4xl mx-auto">
             <div className="py-2 px-5 flex flex-col gap-10 lg:flex-row-reverse mb-5 lg:mt-20">
                 <div className="w-full lg:my-auto">
-                    <MyImage />
+                    <MyImage/>
                 </div>
                 <div>
                     <h1 className="font-bold text-3xl lg:text-5xl">Hi, I’m Amir 👋</h1>
@@ -24,7 +24,8 @@ export const Banner = () => {
                             <BadgeCheck/>
                             <AlertTitle className="lg:text-base">I’m open to full-time remote opportunities</AlertTitle>
                             <AlertDescription className="lg:text-base">
-                                where I can contribute to impactful products, adapt quickly to team needs, and continue growing as an engineer.
+                                where I can contribute to impactful products, adapt quickly to team needs, and continue
+                                growing as an engineer.
                             </AlertDescription>
                         </Alert>
                         <p className="mt-4 text-sm lg:text-base">
@@ -35,12 +36,32 @@ export const Banner = () => {
                             <strong>performance optimization</strong>, and writing code that scales well within teams.
                         </p>
                     </div>
-                    <Button asChild className="block w-full mx-auto text-xs h-max mt-2">
-                        <Link href="https://github.com/amirkarimiii" target="_blank">My GitHub: https://github.com/amirkarimiii</Link>
-                    </Button>
-                    <Button asChild className="block w-full mx-auto text-xs h-max mt-2">
-                        <Link href="https://www.linkedin.com/in/amirhosein-karimkhani/" target="_blank">My Linkedin: /in/amirhosein-karimkhani/</Link>
-                    </Button>
+                    <div className="flex gap-1">
+                        <Button asChild className="flex-1 text-xs mt-2">
+                            <Link
+                                href="https://www.linkedin.com/in/amirhosein-karimkhani/"
+                                target="_blank"
+                                className="flex"
+                            >
+                                Checkout my Github
+                                <span className="w-3">
+                                    <ExternalLink/>
+                                </span>
+                            </Link>
+                        </Button>
+                        <Button asChild className="flex-1 text-xs mt-2">
+                            <Link
+                                href="https://www.linkedin.com/in/amirhosein-karimkhani/"
+                                target="_blank"
+                                className="flex items-center justify-center gap-1"
+                            >
+                                Checkout my Linkedin
+                                <span className="w-3">
+                                    <ExternalLink/>
+                                </span>
+                            </Link>
+                        </Button>
+                    </div>
                     <Button asChild variant="outline" className="block w-full mx-auto text-xs h-max border-4 mt-2">
                         <a href="#contact">Get in Touch</a>
                     </Button>
