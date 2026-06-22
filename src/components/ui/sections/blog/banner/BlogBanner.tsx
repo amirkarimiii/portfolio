e4b07411d57382997344ae031cc8ccf70002f9c0
amir karimi <amir.kk1999@gmail.com>
2026-06-22 23:13:32 +0330

    🔄 refactor(blog): Add banner image to BlogBanner component

--- src/components/ui/sections/blog/banner/BlogBanner.tsx
+++ src/components/ui/sections/blog/banner/BlogBanner.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image';
+
 export function BlogBanner() {
     return (
-        <section className="max-w-4xl mx-auto">
-
+        <section className="max-w-4xl mx-auto h-40 ">
+            <div className="relative w-65 aspect-120/100 mt-5 mx-auto">
+                <Image src={`/me-blog-banner.png`}
+                       alt="amirhosein karimkhani's art-logo"
+                       fill
+                       preload
+                />
+            </div>
+            <p className="rotate-x-55 max-w-max text-2xl font-bold mx-auto">Welcome to my blog</p>
         </section>
     );
 }
