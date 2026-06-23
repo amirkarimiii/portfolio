3db76051572c29b2acb008e6099c847125bdc9b5
amir karimi <amir.kk1999@gmail.com>
2026-06-23 16:35:44 +0330

    🔄 refactor(blog): Make banner image responsive in BlogBanner

--- src/components/ui/sections/blog/banner/BlogBanner.tsx
+++ src/components/ui/sections/blog/banner/BlogBanner.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 export function BlogBanner() {
     return (
         <section className="max-w-4xl mx-auto h-40 ">
-            <div className="relative w-65 aspect-120/100 mt-5 mx-auto">
+            <div className="select-none relative w-65 sm:w-80 md:w-100 aspect-120/100 mt-5 mx-auto">
                 <Image src={`/me-blog-banner.png`}
                        alt="amirhosein karimkhani's art-logo"
                        fill
                        preload
                 />
             </div>
-            <p className="rotate-x-55 max-w-max text-2xl font-bold mx-auto">Welcome to my blog</p>
+            <p className="select-none rotate-x-55 max-w-max text-2xl sm:text-3xl md:text-4xl font-bold mx-auto">Welcome to my blog</p>
         </section>
     );
 }
