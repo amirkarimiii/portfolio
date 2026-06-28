0f56f45a4cb305e3571c51c8e826538340043c8e
amir karimi <amir.kk1999@gmail.com>
2026-06-28 16:43:34 +0330

    feat(admin): add setOpen prop to LoginDialog

--- src/stores/loginDialogStore.ts
+++ src/stores/loginDialogStore.ts
@@ -4,10 +4,12 @@ type LoginDialogStore = {
     open: boolean;
     openDialog: () => void;
     closeDialog: () => void;
+    setOpen: (open: boolean) => void;
 };
 
 export const useLoginDialog = create<LoginDialogStore>((set) => ({
     open: false,
     openDialog: () => set({ open: true }),
     closeDialog: () => set({ open: false }),
+    setOpen: (open: boolean) => set({ open: open }),
 }));
