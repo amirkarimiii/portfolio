765f8f5919154eea787ff7d8f71436b15cc48c0e
amir karimi <amir.kk1999@gmail.com>
2026-06-28 20:11:49 +0330

    ➕ feat(admin): complete LoginDialog UI design

--- src/components/layout/dialogs/LoginDialog.tsx
+++ src/components/layout/dialogs/LoginDialog.tsx
@@ -2,35 +2,47 @@
 
 import {
     Dialog,
-    DialogClose,
     DialogContent,
-    DialogDescription,
     DialogFooter,
     DialogHeader,
     DialogTitle,
-    DialogTrigger
 } from "@/components/ui/shadcn/dialog";
 import {Button} from "@/components/ui/shadcn/button";
+import {useLoginDialog} from "@/stores/loginDialogStore";
+import {Label} from "@/components/ui/shadcn/label";
+import {Field, FieldGroup} from "../../ui/shadcn/field";
+import {InputGroup, InputGroupAddon, InputGroupInput} from "@/components/ui/shadcn/input-group";
+import {Eye, EyeOff} from "lucide-react";
+import React from "react";
 
 export function LoginDialog() {
+
+    const open = useLoginDialog((s) => s.open);
+    const setOpen = useLoginDialog((s) => s.setOpen);
+    const [visible, setVisible] = React.useState(false);
+
     return (
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
             <form>
-                {/*<DialogTrigger asChild>*/}
-                {/*    <Button variant="outline">Open Dialog</Button>*/}
-                {/*</DialogTrigger>*/}
-                <DialogContent className="sm:max-w-sm">
+                <DialogContent className="flex flex-col items-center max-w-sm" showCloseButton={false}>
                     <DialogHeader>
-                        <DialogTitle>Edit profile</DialogTitle>
-                        <DialogDescription>
-                            Login to admin mode
-                        </DialogDescription>
+                        <DialogTitle className="text-center text-2xl">Login</DialogTitle>
                     </DialogHeader>
-                    <DialogFooter>
-                        <DialogClose asChild>
-                            <Button variant="outline">Cancel</Button>
-                        </DialogClose>
-                        <Button type="submit">Login</Button>
+                    <FieldGroup className="mt-5 w-full flex flex-col items-center">
+                        <Field className="w-full sm:w-70">
+                            <Label htmlFor="pass" className="justify-center text-lg">Password</Label>
+                            <InputGroup>
+                                <InputGroupInput type={visible ? "text" : "password"} id="pass" />
+                                <InputGroupAddon align="inline-end" className="cursor-pointer">
+                                    <div className="w-5 h-5" onClick={() => setVisible(!visible)}>
+                                        {visible ? <EyeOff/> : <Eye/>}
+                                    </div>
+                                </InputGroupAddon>
+                            </InputGroup>
+                        </Field>
+                    </FieldGroup>
+                    <DialogFooter className="w-full sm:w-50">
+                            <Button className="w-full" type="submit">Login</Button>
                     </DialogFooter>
                 </DialogContent>
             </form>
