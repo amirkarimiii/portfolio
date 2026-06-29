98a739f12706e52fdf3633c8bb51691982979b84
amir karimi <amir.kk1999@gmail.com>
2026-06-29 22:57:36 +0330

    feat(navbar): redesign action layout in navbar components

--- src/components/layout/navbar/BlogNavbarActions.tsx
+++ src/components/layout/navbar/BlogNavbarActions.tsx
@@ -1,12 +1,18 @@
 import {ThemeButton} from "@/components/ui/buttons/ThemeButton";
 import {BlogNavSearchbar} from "@/components/ui/inputs/BlogNavSearchbar";
 import {HomeButton} from "@/components/ui/buttons/HomeButton";
+import {LogoutButton} from "@/components/ui/buttons/LogoutButton";
+import {NewArticleButton} from "@/components/ui/buttons/NewArticleButton";
 
 export function BlogNavbarActions() {
     return (
         <>
-            <ThemeButton/>
             <div className="flex flex-row gap-1">
+                <ThemeButton/>
+                <LogoutButton/>
+            </div>
+            <div className="flex flex-row gap-1">
+                <NewArticleButton/>
                 <BlogNavSearchbar/>
                 <HomeButton />
             </div>
--- src/components/layout/navbar/MainNavbarActions.tsx
+++ src/components/layout/navbar/MainNavbarActions.tsx
@@ -1,11 +1,19 @@
 import {ThemeButton} from "@/components/ui/buttons/ThemeButton";
 import {CvButton} from "@/components/ui/buttons/CVButton";
+import {LogoutButton} from "@/components/ui/buttons/LogoutButton";
+import {NewArticleButton} from "@/components/ui/buttons/NewArticleButton";
 
 export function MainNavbarActions() {
     return (
         <>
-            <ThemeButton/>
-            <CvButton/>
+            <div className="flex flex-row gap-1">
+                <ThemeButton/>
+                <NewArticleButton/>
+            </div>
+            <div className="flex flex-row gap-1">
+                <CvButton/>
+                <LogoutButton/>
+            </div>
         </>
     );
 }
