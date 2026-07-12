e25946501fc5f31212228971ee8d0c88db5a3cc3
amir karimi <amir.kk1999@gmail.com>
2026-07-12 22:03:27 +0330

    feat(editor): add TiptapSkeleton component

---
+++ src/components/ui/sections/add-article/editor/TiptapSkeleton.tsx
@@ -0,0 +1,17 @@
+import {Skeleton} from "@/components/ui/shadcn/skeleton";
+
+export default function TiptapSkeleton() {
+    return (
+        <div className="mt-10 w-full rounded-md border p-4">
+            <div className="min-h-[300px] space-y-3">
+                <Skeleton className="h-4 w-3/4" />
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-5/6" />
+                <Skeleton className="h-4 w-full" />
+                <Skeleton className="h-4 w-2/3" />
+                <Skeleton className="h-4 w-4/5" />
+                <Skeleton className="h-4 w-1/2" />
+            </div>
+        </div>
+    );
+}
