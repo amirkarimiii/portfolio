3c64bf7e64b99b7acc7664ad7c523bdc7b5499db
amir karimi <amir.kk1999@gmail.com>
2026-07-13 17:35:50 +0330

    feat(editor): add text type selector button to Toolbar

--- src/components/ui/sections/add-article/editor/toolbar/Toolbar.tsx
+++ src/components/ui/sections/add-article/editor/toolbar/Toolbar.tsx
@@ -1,7 +1,7 @@
 import {Editor} from "@tiptap/react";
-import {ButtonGroup} from "@/components/ui/shadcn/button-group";
+import {ButtonGroup, ButtonGroupSeparator} from "@/components/ui/shadcn/button-group";
 import {Button} from "@/components/ui/shadcn/button";
-import {RedoIcon, UndoIcon} from "lucide-react";
+import {ChevronDownIcon, RedoIcon, UndoIcon} from "lucide-react";
 
 function Toolbar({editor}: { editor: Editor | null }) {
     if (!editor) return null
@@ -17,6 +17,17 @@ function Toolbar({editor}: { editor: Editor | null }) {
                 <Button variant="ghost" className="w-max px-2">
                     <div className="w-4 aspect-square">
                         <RedoIcon />
+                    </div>
+                </Button>
+            </ButtonGroup>
+            <ButtonGroupSeparator/>
+            <ButtonGroup>
+                <Button variant="ghost" className="w-max px-2">
+                    Paragraph
+                </Button>
+                <Button variant="ghost" className="w-max px-2">
+                    <div className="w-3 aspect-square">
+                        <ChevronDownIcon/>
                     </div>
                 </Button>
             </ButtonGroup>
