3ac70a82b7df666da0d1510cd4de5855c6b9adb1
amir karimi <amir.kk1999@gmail.com>
2026-07-15 18:03:24 +0330

    refactor(editor): remove simple and advance tabs from EditorSection

--- src/components/ui/sections/add-article/editor/EditorSection.tsx
+++ src/components/ui/sections/add-article/editor/EditorSection.tsx
@@ -5,7 +5,6 @@ import Highlight from '@tiptap/extension-highlight'
 import Tiptap from './Tiptap'
 import {Button} from "@/components/ui/shadcn/button";
 import SimpleToolbar from "./toolbar/SimpleToolbar";
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/shadcn/tabs";
 
 const EditorSection = () => {
     const editor = useEditor({
@@ -26,29 +25,17 @@ const EditorSection = () => {
     const handleLogContent = () => {
         if (!editor) return
         console.log(editor.getJSON());
+        console.log(editor.getHTML());
     }
 
     return (
         <section className="max-w-4xl mx-auto max-h-max mt-8">
             <Button onClick={handleLogContent}>log</Button>
             <div className="flex flex-col">
-                <Tabs defaultValue="simple" className="w-max self-center sticky top-2 z-10 mt-10">
-                    <TabsList className="flex flex-wrap h-max">
-                        <TabsTrigger value="simple">
-                            <span className="text-xs">simple</span>
-                        </TabsTrigger>
-                        <TabsTrigger value="advance">
-                            <span className="text-xs">advance</span>
-                        </TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="simple">
-                        <div className="w-full bg-background  rounded-md border p-1 shadow-lg">
-                            <SimpleToolbar editor={editor} />
-                        </div>
-                    </TabsContent>
-                </Tabs>
+                <div className="w-max self-center sticky top-2 z-10 mt-10 bg-background rounded-md border p-1 shadow-lg">
+                    <SimpleToolbar editor={editor} />
+                </div>
                 <Tiptap editor={editor}/>
-                <Button className="self-center w-sm mt-3 mb-15" onClick={handleLogContent}>log</Button>
             </div>
         </section>
     )
