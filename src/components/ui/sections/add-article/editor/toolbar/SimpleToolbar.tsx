e95d9fc9aeea9ecabd93716cd2cc473db71875e7
amir karimi <amir.kk1999@gmail.com>
2026-07-15 18:22:13 +0330

    fix(editor): revise undo and redo button styles in SimpleToolbar

--- src/components/ui/sections/add-article/editor/toolbar/SimpleToolbar.tsx
+++ src/components/ui/sections/add-article/editor/toolbar/SimpleToolbar.tsx
@@ -133,13 +133,13 @@ function SimpleToolbar({editor}: { editor: Editor | null }) {
     return (
         <ButtonGroup>
             <ButtonGroup>
-                <Button variant="ghost" className="w-max px-2" onClick={() => editor?.chain().focus().undo().run()}>
-                    <div className="w-4 aspect-square">
+                <Button variant="ghost" className="w-max px-1.5" onClick={() => editor?.chain().focus().undo().run()}>
+                    <div className="w-3.5 aspect-square">
                         <UndoIcon/>
                     </div>
                 </Button>
-                <Button variant="ghost" className="w-max px-2" onClick={() => editor?.chain().focus().redo().run()}>
-                    <div className="w-4 aspect-square">
+                <Button variant="ghost" className="w-max px-1.5" onClick={() => editor?.chain().focus().redo().run()}>
+                    <div className="w-3.5 aspect-square">
                         <RedoIcon/>
                     </div>
                 </Button>
