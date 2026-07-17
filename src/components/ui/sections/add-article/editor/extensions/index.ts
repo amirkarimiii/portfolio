70653c817dafdbada979af479b063bb3cb983090
amir karimi <amir.kk1999@gmail.com>
2026-07-17 22:04:38 +0330

    feat(editor) add list extension

--- src/components/ui/sections/add-article/editor/extensions/index.ts
+++ src/components/ui/sections/add-article/editor/extensions/index.ts
@@ -2,10 +2,12 @@ import {coreExtensions} from "./core-extensions";
 import {historyExtensions} from "./history-extensions";
 import {headingExtensions} from "./heading-extensions";
 import {blockExtensions} from "./block-extensions";
+import {listExtensions} from "@/components/ui/sections/add-article/editor/extensions/list-extention";
 
 export const extensions = [
     ...coreExtensions,
     ...historyExtensions,
     ...headingExtensions,
-    ...blockExtensions
+    ...blockExtensions,
+    ...listExtensions
 ]
---
+++ src/components/ui/sections/add-article/editor/extensions/list-extention.ts
@@ -0,0 +1,9 @@
+import {BulletList} from "@tiptap/extension-bullet-list";
+import {OrderedList} from "@tiptap/extension-ordered-list";
+import {ListItem} from "@tiptap/extension-list-item";
+
+export const listExtensions = [
+    BulletList,
+    OrderedList,
+    ListItem,
+]
