5c082abe168fca8a8af9ad17d5eaf546b6d0fb50
amir karimi <amir.kk1999@gmail.com>
2026-07-17 19:24:50 +0330

    feat(editor): add block (code block & block quote) extension

---
+++ src/components/ui/sections/add-article/editor/extensions/block-extensions.ts
@@ -0,0 +1,7 @@
+import Blockquote from "@tiptap/extension-blockquote";
+import CodeBlock from "@tiptap/extension-code-block";
+
+export const blockExtensions = [
+    Blockquote,
+    CodeBlock,
+]
--- src/components/ui/sections/add-article/editor/extensions/index.ts
+++ src/components/ui/sections/add-article/editor/extensions/index.ts
@@ -1,9 +1,11 @@
 import {coreExtensions} from "./core-extensions";
 import {historyExtensions} from "./history-extensions";
 import {headingExtensions} from "./heading-extensions";
+import {blockExtensions} from "./block-extensions";
 
 export const extensions = [
     ...coreExtensions,
     ...historyExtensions,
     ...headingExtensions,
+    ...blockExtensions
 ]
