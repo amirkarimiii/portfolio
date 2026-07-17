db874b753e17deb02654337067a0297346d68f44
amir karimi <amir.kk1999@gmail.com>
2026-07-17 17:25:52 +0330

    feat(editor): add hint labels to HistoryButton config

--- src/components/ui/sections/add-article/editor/toolbar/HistoryButton/history.ts
+++ src/components/ui/sections/add-article/editor/toolbar/HistoryButton/history.ts
@@ -5,9 +5,11 @@ export const HISTORY = {
     undo: {
         icon: UndoIcon,
         command: (editor: Editor | null) => editor?.chain().focus().undo().run(),
+        hint: "Undo",
     },
     redo: {
         icon: RedoIcon,
         command: (editor: Editor | null) => editor?.chain().focus().redo().run(),
+        hint: "Redo",
     },
 } as const;
