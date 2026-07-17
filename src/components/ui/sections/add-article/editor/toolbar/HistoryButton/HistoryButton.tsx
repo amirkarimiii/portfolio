a404bf3cc12cdadaca59d0b30c1dd847f8c51466
amir karimi <amir.kk1999@gmail.com>
2026-07-17 17:26:51 +0330

    feat(editor): add tooltip to HistoryButton

--- src/components/ui/sections/add-article/editor/toolbar/HistoryButton/HistoryButton.tsx
+++ src/components/ui/sections/add-article/editor/toolbar/HistoryButton/HistoryButton.tsx
@@ -1,6 +1,7 @@
 import {Editor} from "@tiptap/react";
 import {Button} from "@/components/ui/shadcn/button";
 import {HISTORY} from "./history";
+import {Tooltip, TooltipContent, TooltipTrigger} from "@/components/ui/shadcn/tooltip";
 
 interface HistoryButtonProps {
     type: "undo" | "redo";
@@ -9,17 +10,24 @@ interface HistoryButtonProps {
 
 export function HistoryButton({type, editor}: HistoryButtonProps) {
 
-    const { icon: Icon, command } = HISTORY[type];
+    const { icon: Icon, command, hint } = HISTORY[type];
 
     return (
-        <Button
-            variant="ghost"
-            className="w-max px-1.5"
-            onClick={() => command(editor)}
-        >
-            <div className="w-4 aspect-square">
-                <Icon />
-            </div>
-        </Button>
+        <Tooltip>
+            <TooltipTrigger asChild>
+                <Button
+                    variant="ghost"
+                    className="w-max px-1.5"
+                    onClick={() => command(editor)}
+                >
+                    <div className="w-4 aspect-square">
+                        <Icon />
+                    </div>
+                </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+                {hint}
+            </TooltipContent>
+        </Tooltip>
     );
 }
