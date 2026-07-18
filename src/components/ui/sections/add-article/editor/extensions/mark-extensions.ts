29a536be9e13d5b5430fae75345817c000090435
amir karimi <amir.kk1999@gmail.com>
2026-07-18 17:35:10 +0330

    feat(editor) add strike extension

--- src/components/ui/sections/add-article/editor/extensions/mark-extensions.ts
+++ src/components/ui/sections/add-article/editor/extensions/mark-extensions.ts
@@ -1,7 +1,9 @@
 import Bold from '@tiptap/extension-bold'
 import Italic from "@tiptap/extension-italic";
+import Strike from "@tiptap/extension-strike";
 
 export const markExtensions = [
     Bold,
     Italic,
+    Strike,
 ]
