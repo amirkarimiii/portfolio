656f04823187de9ce3532723ded1821fc3863c00
amir karimi <amir.kk1999@gmail.com>
2026-07-18 17:45:56 +0330

    feat(editor) add highlight extension

--- src/components/ui/sections/add-article/editor/extensions/mark-extensions.ts
+++ src/components/ui/sections/add-article/editor/extensions/mark-extensions.ts
@@ -2,10 +2,12 @@ import Bold from '@tiptap/extension-bold'
 import Italic from "@tiptap/extension-italic";
 import Strike from "@tiptap/extension-strike";
 import Underline from "@tiptap/extension-underline";
+import Highlight from "@tiptap/extension-highlight";
 
 export const markExtensions = [
     Bold,
     Italic,
     Strike,
     Underline,
+    Highlight,
 ]
