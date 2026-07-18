ed1a8f44419725902bf34bbe67f91109591cd330
amir karimi <amir.kk1999@gmail.com>
2026-07-18 17:40:07 +0330

    feat(editor) add underline extension

--- src/components/ui/sections/add-article/editor/extensions/mark-extensions.ts
+++ src/components/ui/sections/add-article/editor/extensions/mark-extensions.ts
@@ -1,9 +1,11 @@
 import Bold from '@tiptap/extension-bold'
 import Italic from "@tiptap/extension-italic";
 import Strike from "@tiptap/extension-strike";
+import Underline from "@tiptap/extension-underline";
 
 export const markExtensions = [
     Bold,
     Italic,
     Strike,
+    Underline,
 ]
