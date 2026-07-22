d60c13e6a00e0ce04472de9b4d5551317bc14d94
amir karimi <amir.kk1999@gmail.com>
2026-07-22 21:16:18 +0330

    🐞 fix(admin): use named export in session route

--- src/app/api/admin/session/route.ts
+++ src/app/api/admin/session/route.ts
@@ -1,9 +1,9 @@
-import {NextRequest, NextResponse} from 'next/server';
+import {NextResponse} from 'next/server';
 import { cookies } from 'next/headers';
 import clientPromise from '@/lib/mongodb';
 
 
-export default async function GET(request: NextRequest) {
+export async function GET() {
 
     try {
 
