56ee9ef0d8e5c87ec9e557e89df11ecf5241071e
amir karimi <amir.kk1999@gmail.com>
2026-07-26 17:59:24 +0330

    🐞 fix: update imports in src/features/blog/components/BlogBanner.tsx to match new directory structure

--- src/features/blog/components/BlogBanner.tsx
+++ src/features/blog/components/BlogBanner.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import {Separator} from "@/components/ui/shadcn/separator";
+import {Separator} from "@/shared/components/ui/separator";
 
 export function BlogBanner() {
     return (
