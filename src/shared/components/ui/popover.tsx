c719010ce077a08f42076396355ee2d499d0c00e
amir karimi <amir.kk1999@gmail.com>
2026-08-08 22:33:08 +0330

    🐞 fix(rebase): fix imports in ui/popover relocation

--- src/shared/components/ui/popover.tsx
+++ src/shared/components/ui/popover.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react"
 import { Popover as PopoverPrimitive } from "radix-ui"
+import {cn} from "@/shared/utils/shadcnUtils";
 
-import { cn } from "@/lib/utils"
 
 function Popover({
   ...props
