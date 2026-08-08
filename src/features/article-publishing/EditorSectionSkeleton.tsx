c73ddfe3c85a8750ed12c992ff19bfc832ff3f31
amir karimi <amir.kk1999@gmail.com>
2026-08-08 20:36:27 +0330

    ♻️ refactor(rebase): move EditorSectionSkeleton to article-publishing feature

--- src/features/article-publishing/EditorSectionSkeleton.tsx
+++ src/features/article-publishing/EditorSectionSkeleton.tsx
@@ -3,7 +3,7 @@ import {Skeleton} from "@/components/ui/shadcn/skeleton";
 export default function EditorSectionSkeleton() {
     return (
         <div className="mt-10 w-full rounded-md border p-4">
-            <div className="min-h-[300px] space-y-3">
+            <div className="min-h-75 space-y-3">
                 <Skeleton className="h-4 w-3/4" />
                 <Skeleton className="h-4 w-full" />
                 <Skeleton className="h-4 w-5/6" />
