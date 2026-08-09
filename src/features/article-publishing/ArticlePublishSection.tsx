ddb46214853cbbb5ccbf43f8e749f3b48bc585a9
amir karimi <amir.kk1999@gmail.com>
2026-08-09 19:49:39 +0330

    ➕ feat(blog): add main tabs to ArticlePublishSection

--- src/features/article-publishing/ArticlePublishSection.tsx
+++ src/features/article-publishing/ArticlePublishSection.tsx
@@ -1,6 +1,20 @@
-'use client'
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/shared/components/ui/tabs"
 
 const ArticlePublishSection = () => {
 
+    return (
+        <section className="max-w-4xl mx-auto max-h-max mt-8">
+            <Tabs defaultValue="metadata" className="w-full">
+                <TabsList className="w-full ">
+                    <TabsTrigger value="metadata">Metadata</TabsTrigger>
+                    <TabsTrigger value="content">Content</TabsTrigger>
+                    <TabsTrigger value="related">Related Articles</TabsTrigger>
+                </TabsList>
+                <TabsContent value="metadata">Metadata</TabsContent>
+                <TabsContent value="content">Content</TabsContent>
+                <TabsContent value="related">Related Articles</TabsContent>
+            </Tabs>
+        </section>
+    )
 }
 export default ArticlePublishSection
