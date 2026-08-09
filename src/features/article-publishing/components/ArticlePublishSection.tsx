0ea179343de704d11d0dba438087adaf3d971390
amir karimi <amir.kk1999@gmail.com>
2026-08-09 20:20:27 +0330

    ➕ feat(blog): add initial header section to ArticlePublishSection

--- src/features/article-publishing/components/ArticlePublishSection.tsx
+++ src/features/article-publishing/components/ArticlePublishSection.tsx
@@ -1,9 +1,24 @@
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/shared/components/ui/tabs";
+import {CircleCheck, CircleX, Clock} from "lucide-react";
 
 const ArticlePublishSection = () => {
 
+    const status = {
+        success: [<CircleCheck key="success" color="green" />, "saved as draft!"],
+        failed: [<CircleX key="failed" color="red" />, "failed to save!"],
+        pending: [<Clock key="pending" color="gray" />, "pending"],
+    } as const;
+
     return (
-        <section className="max-w-4xl mx-auto max-h-max mt-8">
+        <section className="max-w-4xl mx-auto max-h-max mt-8 ">
+            <div className="w-full h-12 px-5 py-3 flex flex-row gap-2">
+                <div className="w-5 aspect-square mt-1">
+                    {status.success[0]}
+                </div>
+                <p>
+                    {status.success[1]}
+                </p>
+            </div>
             <Tabs defaultValue="metadata" className="w-full">
                 <TabsList className="w-full ">
                     <TabsTrigger value="metadata">Metadata</TabsTrigger>
