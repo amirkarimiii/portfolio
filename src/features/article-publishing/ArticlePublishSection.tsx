0b059a71f60eb0b2a4a54e95397cb437ab063459
amir karimi <amir.kk1999@gmail.com>
2026-08-09 18:52:39 +0330

    ♻️ refactor(blog): rename EditorSection to ArticlePublishSection and clean up legacy code

--- src/features/article-publishing/ArticlePublishSection.tsx
+++ src/features/article-publishing/ArticlePublishSection.tsx
@@ -1,39 +1,6 @@
 'use client'
-import {useEditor} from '@tiptap/react'
-import Tiptap from './article-editor/Tiptap'
-import Toolbar from "./article-editor/toolbar/Toolbar";
-import {extensions} from "@/features/article-publishing/article-editor/extensions";
-import {Button} from "@/shared/components/ui/button";
 
-const ArticlePublish = () => {
-    const editor = useEditor({
-        extensions: [
-            ...extensions
-        ],
-        immediatelyRender: false,
-        editorProps: {
-            attributes: {
-                class: 'mt-3 min-h-75 w-full rounded-md border p-4 focus:outline-none mx-auto',
-            },
-        },
-    })
+const ArticlePublishSection = () => {
 
-    const handleLogContent = () => {
-        if (!editor) return
-        console.log(editor.getJSON());
-        console.log(editor.getHTML());
-    }
-
-    return (
-        <section className="max-w-4xl mx-auto max-h-max mt-8">
-            <Button onClick={handleLogContent}>log</Button>
-            <div className="flex flex-col">
-                <div className="w-max self-center sticky top-2 z-10 mt-10 bg-background rounded-md border p-1 shadow-lg">
-                    <Toolbar editor={editor} />
-                </div>
-                <Tiptap editor={editor}/>
-            </div>
-        </section>
-    )
 }
-export default ArticlePublish
+export default ArticlePublishSection
