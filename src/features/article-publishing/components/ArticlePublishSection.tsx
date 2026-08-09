4a2dfb15b54823b5f7b039873fada890e41ab668
amir karimi <amir.kk1999@gmail.com>
2026-08-09 21:32:15 +0330

    ➕ feat(blog): complete header in ArticlePublishSection

--- src/features/article-publishing/components/ArticlePublishSection.tsx
+++ src/features/article-publishing/components/ArticlePublishSection.tsx
@@ -1,23 +1,44 @@
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/shared/components/ui/tabs";
 import {CircleCheck, CircleX, Clock} from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/shared/components/ui/dropdown-menu";
+import {Button} from "@/shared/components/ui/button";
 
 const ArticlePublishSection = () => {
 
     const status = {
-        success: [<CircleCheck key="success" color="green" />, "saved as draft!"],
-        failed: [<CircleX key="failed" color="red" />, "failed to save!"],
-        pending: [<Clock key="pending" color="gray" />, "pending"],
+        success: [<CircleCheck key="success" color="green"/>, "saved as draft!"],
+        failed: [<CircleX key="failed" color="red"/>, "failed to save!"],
+        pending: [<Clock key="pending" color="gray"/>, "pending"],
     } as const;
 
     return (
-        <section className="max-w-4xl mx-auto max-h-max mt-8 ">
-            <div className="w-full h-12 px-5 py-3 flex flex-row gap-2">
-                <div className="w-5 aspect-square mt-1">
-                    {status.success[0]}
+        <section className="max-w-4xl mx-auto max-h-max mt-8">
+            <div className="w-full flex flex-row justify-between px-5 py-2">
+                <div className="w-full h-max flex flex-row gap-2 my-auto">
+                    <div className="w-5 aspect-square">
+                        {status.success[0]}
+                    </div>
+                    <p>
+                        {status.success[1]}
+                    </p>
                 </div>
-                <p>
-                    {status.success[1]}
-                </p>
+                <div className="my-auto">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline">Action</Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuItem className="justify-center">Archive</DropdownMenuItem>
+                            <DropdownMenuItem className="justify-center">Delete</DropdownMenuItem>
+                            <DropdownMenuItem className="justify-center">Publish</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </div>
             <Tabs defaultValue="metadata" className="w-full">
                 <TabsList className="w-full ">
