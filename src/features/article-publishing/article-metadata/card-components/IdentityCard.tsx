c82dc0b74413903e37a54dd53991799a572b9acc
amir karimi <amir.kk1999@gmail.com>
2026-08-11 18:43:19 +0330

    ➕ feat(blog): integrate react-hook-form with zodResolver in IdentityCard

--- src/features/article-publishing/article-metadata/card-components/IdentityCard.tsx
+++ src/features/article-publishing/article-metadata/card-components/IdentityCard.tsx
@@ -1,6 +1,35 @@
+'use client';
+
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {
+    identitySchema,
+    type IdentityFormValues,
+} from '@/features/article-publishing/schemas/identitySchema';
+import {Form} from "@/shared/components/ui/form";
+
+const defaultValues: IdentityFormValues = {
+    title: '',
+    slug: '',
+    summary: ''
+};
+
 export function IdentityCard() {
+    const form = useForm<IdentityFormValues>({
+        resolver: zodResolver(identitySchema),
+        defaultValues,
+        mode: 'onChange',
+    });
+
+    function onSubmit(data: IdentityFormValues) {
+        console.log('Identity Form Data:', data);
+    }
+
     return (
-        <>
-        </>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+
+            </form>
+        </Form>
     );
 }
