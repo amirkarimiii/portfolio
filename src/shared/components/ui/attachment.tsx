7e8beedea2641f72d45fae6477f6da3111a45bd0
amir karimi <amir.kk1999@gmail.com>
2026-08-13 17:51:54 +0330

    🐞 fix(ui): correct imports in attachment component

--- src/shared/components/ui/attachment.tsx
+++ src/shared/components/ui/attachment.tsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 import { Slot } from "radix-ui"
+import {cn} from "@/shared/utils/shadcnUtils";
+import {Button} from "@/shared/components/ui/button";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
 
 const attachmentVariants = cva(
   "group/attachment relative flex w-fit max-w-full min-w-0 shrink-0 flex-wrap rounded-xl border bg-card text-card-foreground transition-colors focus-within:ring-1 focus-within:ring-ring/50 has-[>a,>button]:hover:bg-muted/50 data-[state=error]:border-destructive/30 data-[state=idle]:border-dashed",
