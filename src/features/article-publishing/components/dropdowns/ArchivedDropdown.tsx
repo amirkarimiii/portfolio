297494933a95364fff5895acca92662268a58025
amir karimi <amir.kk1999@gmail.com>
2026-08-19 22:13:39 +0330

    ➕ feat(blog): add ArchivedDropdown component for archive article actions

---
+++ src/features/article-publishing/components/dropdowns/ArchivedDropdown.tsx
@@ -0,0 +1,47 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/shared/components/ui/dropdown-menu";
+import {Button} from "@/shared/components/ui/button";
+import {Settings} from "lucide-react";
+
+type Props = {
+    uniqueId: string;
+};
+
+export function ArchivedDropdown({ uniqueId }: Props) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant="outline"
+                    className="w-max h-max rounded-md p-1 cursor-pointer self-end"
+                    data-no-card-navigate
+                >
+                    <div className="w-4 aspect-square">
+                        <Settings/>
+                    </div>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="flex flex-col gap-0.5">
+                <DropdownMenuItem onClick={() => {
+                    console.log("edit action")
+                }} className="justify-center">
+                    Edit
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => {
+                    console.log("archive action")
+                }} className="justify-center">
+                    Publish
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => {
+                    console.log("safe delete action")
+                }} variant="destructive" className="justify-center">
+                    Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
