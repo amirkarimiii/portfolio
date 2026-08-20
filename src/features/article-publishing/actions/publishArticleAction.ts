b8d128bf971833d857b02935ded660219fe764ed
amir karimi <amir.kk1999@gmail.com>
2026-08-20 19:31:44 +0330

    ➕ feat(blog): implement slug uniqueness check in publishArticleAction

--- src/features/article-publishing/actions/publishArticleAction.ts
+++ src/features/article-publishing/actions/publishArticleAction.ts
@@ -5,13 +5,21 @@ import {ArticleRepository} from "@/features/article-publishing/repository/articl
 
 export async function publishArticleAction(formData: ArticleFormValues) {
     try {
+        const slugExists = await ArticleRepository.isSlugExists(formData.slug);
+        if (slugExists) {
+            return {
+                success: false,
+                error: 'this slug already exists',
+                field: 'slug',
+            };
+        }
         const newArticle = await ArticleRepository.savePublishedArticle(formData);
         return { success: true, data: newArticle };
     } catch (error) {
         console.error('Failed to publish article:', error);
         return {
             success: false,
-            error: error instanceof Error ? error.message : 'Unknown error occurred'
+            error: error instanceof Error ? error.message : 'something went wrong',
         };
     }
 }
