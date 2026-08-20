e744a086d41d0bc067ed01b52ae87be67b8e2665
amir karimi <amir.kk1999@gmail.com>
2026-08-20 14:51:37 +0330

    🎨 style(blog): refine article MetadataTab section header styling

--- src/features/article-publishing/components/article/article-metadata/MetadataTab.tsx
+++ src/features/article-publishing/components/article/article-metadata/MetadataTab.tsx
@@ -13,24 +13,24 @@ export function MetadataTab() {
     return (
         <div className="flex flex-col gap-3 pb-20">
             <div>
-                <div>
-                    <h2 className="font-bold text-xl my-5 ml-2 md:ml-0 lg:mt-5 lg:text-3xl">Identity</h2>
+                <div className="text-center">
+                    <h2 className="font-bold opacity-80 text-md my-5 ml-2 md:ml-0 lg:mt-5 lg:text-xl">Identity</h2>
                 </div>
                 <IdentityCard/>
                 <Separator/>
             </div>
 
             <div>
-                <div>
-                    <h2 className="font-bold text-xl my-5 ml-2 md:ml-0 lg:mt-5 lg:text-3xl">Assets</h2>
+                <div className="text-center">
+                    <h2 className="font-bold opacity-80 text-md my-5 ml-2 md:ml-0 lg:mt-5 lg:text-xl">Assets</h2>
                 </div>
                 <BaseAssetsForm />
                 <Separator/>
             </div>
 
             <div>
-                <div>
-                    <h2 className="font-bold text-xl my-5 ml-2 md:ml-0 lg:mt-5 lg:text-3xl">Classification</h2>
+                <div className="text-center">
+                    <h2 className="font-bold opacity-80 text-md my-5 ml-2 md:ml-0 lg:mt-5 lg:text-xl">Classification</h2>
                 </div>
                 <ClassificationCard/>
                 <Separator/>
@@ -38,8 +38,8 @@ export function MetadataTab() {
             </div>
 
             <div>
-                <div>
-                    <h2 className="font-bold text-xl my-5 ml-2 md:ml-0 lg:mt-5 lg:text-3xl">SEO</h2>
+                <div className="text-center">
+                    <h2 className="font-bold opacity-80 text-md my-5 ml-2 md:ml-0 lg:mt-5 lg:text-xl">SEO</h2>
                 </div>
                 <BaseSEOForm entityType="article" />
             </div>
