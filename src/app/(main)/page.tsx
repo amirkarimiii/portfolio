406685709d0542815a3debd71e0f1418c57ee772
amir karimi <amir.kk1999@gmail.com>
2026-09-04 13:40:35 +0330

    ➕ feat(stack): add StackSection to main homepage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,12 +2,14 @@ import {Banner} from "@/features/main/Banner";
 import {InfoSection} from "@/features/main/infoSection";
 import {ProjectsSection} from "@/features/main/ProjectsSection";
 import {ContactSection} from "@/features/main/ContactSection";
+import {StackSection} from "@/features/stack-mapping/components/StackSection";
 
 
 export default function Home() {
     return (
         <>
             <Banner/>
+            <StackSection/>
             <InfoSection/>
             <ProjectsSection/>
             <ContactSection/>
